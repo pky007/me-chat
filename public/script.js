dc5325ed60fb8aedef0f4e889c5c00ebeaa3b113
your mobcoder-ashu <pankaj.yadav@mobcoder.com>
2020-08-07 09:07:05 +0530

    init

---
+++ public/script.js
@@ -0,0 +1,132 @@
+
+
+const socket = io('/')
+
+const videoGrid = document.getElementById("video-grid")
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+var peer = new Peer(undefined, {
+    path: 'peerjs',
+    host: '/',
+    port: 3050
+});
+
+let myVideoStream
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    myVideoStream = stream
+    addVideoStream(myVideo, stream)
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+
+    })
+
+
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream)
+    })
+
+
+    let text = $('input')
+    $('html').keydown(e => {
+
+        if (e.which == 13 && text.val().length !== 0) {
+            socket.emit('message', text.val());
+            text.val('')
+        }
+    })
+
+    socket.on('createMessage', message => {
+        $('.messages').append(`<li class="message"><b>user: </b>${message}</li>`)
+        //    scrollToBottom()
+    })
+
+})
+
+// const scrollToBottom = ()=>{
+//     var d = $('.main_chat_window');
+//     d.scrollTop(d.prop("scrollHeight"))
+
+// }
+
+const muteUnMute = () => {
+  
+    const enable = myVideoStream.getAudioTracks()[0].enable
+    console.log("call", enable)
+    if (enable) {
+        myVideoStream.getAudioTracks()[0].enable = false
+        setUnMuteButton()
+    }
+    else {
+        setMuteButton()
+        myVideoStream.getAudioTracks()[0].enable = true
+    }
+}
+
+
+const  setMuteButton = ()=>{
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Unmute</span>`
+    document.querySelector('.main_mute_button').innerHeight = html
+}
+
+
+
+
+const  setUnMuteButton = ()=>{
+    const html = `
+    <i class="fas fa-microphone-slash"></i>
+    <span>Unmute</span>`
+    document.querySelector('.main_mute_button').innerHeight = html
+}
+
+
+
+
+
+
+
+
+
+
+
+
+peer.on('open', id => {
+    socket.emit('join-room', Room_ID, id)
+})
+
+
+
+const connectToNewUser = (userId, stream) => {
+    var call = peer.call(userId, stream);
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+
+
+}
+
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+
+    })
+
+    videoGrid.append(video)
+
+
+}
+
---
+++ server.js
@@ -0,0 +1,44 @@
+// import * as uuid from 'uuid/v4';
+const express = require("express")
+const app = express()
+const server = require('http').Server(app)
+const io = require("socket.io")(server)
+const uuid = require("uuid").v4
+const { ExpressPeerServer } = require('peer');
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+  });
+   
+
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use('/peerjs', peerServer)
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuid()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render("room", { roomId: req.params.room })
+})
+
+io.on('connection', socket =>{
+    console.log("socket connected")
+    socket.on('join-room',(roomId, userId)=>{       
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+
+        socket.on('message', message =>{
+            io.to(roomId).emit('createMessage', message)
+        })
+
+
+    })
+    
+})
+
+server.listen(3050)
